67d242be868b71eeb48ae714bb6593a2f7cce78c
kgjsid <kgjsid@gmail.com>
2024-05-11 12:15:08 +0900

    feat : test 캐릭터 제작
    Hat, Clothes 등 다양하게 커스텀 할 수 있는 캐릭터 제작

---
+++ Assets/_Seo/Scripts/TestScripts/TempPlayerAttack.cs
@@ -0,0 +1,14 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerAttack : MonoBehaviour
+{
+    [PunRPC]
+    private void RequestAttack()
+    {
+        // 마스터에게 어택 요청
+    }
+
+}
