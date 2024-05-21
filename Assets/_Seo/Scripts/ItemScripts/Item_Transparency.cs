460d3c130daecad141c0152200b7144da74751d0
kgjsid <kgjsid@gmail.com>
2024-05-21 17:33:14 +0900

    feat : 투명화 아이템 제작
    가능은 하나 조금 더 수정이 필요함

---
+++ Assets/_Seo/Scripts/ItemScripts/Item_Transparency.cs
@@ -0,0 +1,83 @@
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Transparency : MonoBehaviour
+{
+    // 투명화 아이템
+    [SerializeField] materials normalMaterial;
+    [SerializeField] materials transparentMaterial;  // 완전 투명
+    [SerializeField] materials translucentMaterial;  // 반투명
+
+    [SerializeField] PlayerController user;
+    [SerializeField] SkinnedMeshRenderer render;
+
+    [SerializeField] float setTime;
+    List<Material> materiallist = new List<Material>();
+
+    [SerializeField] LayerMask transparentLayer;
+    [SerializeField] LayerMask translucentLayer;
+    [SerializeField] LayerMask normalLayer;
+
+    public void SetUser(PlayerController user)
+    {
+        this.user = user;
+        render = user.gameObject.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetTransparent();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetTranslucent();
+        }
+    }
+
+    public void Use()
+    {
+        
+    }
+
+    private void SetTransparent()
+    {   // 불투명 설정
+        materiallist.Clear();
+        materiallist.Add(transparentMaterial.skinMaterial); materiallist.Add(transparentMaterial.baseMaterial);
+        //user.gameObject.layer = 30;
+
+        render.SetMaterials(materiallist);
+        StartCoroutine(SettingTime());
+    }
+
+    private void SetTranslucent()
+    {   // 반투명 설정
+        materiallist.Clear();
+        //user.gameObject.layer = 31;
+        materiallist.Add(translucentMaterial.skinMaterial); materiallist.Add(translucentMaterial.baseMaterial);
+
+        render.SetMaterials(materiallist);
+        StartCoroutine(SettingTime());
+    }
+
+    IEnumerator SettingTime()
+    {
+        yield return new WaitForSeconds(setTime);
+        materiallist.Clear();
+        //user.gameObject.layer = 3;
+        materiallist.Add(normalMaterial.skinMaterial); materiallist.Add(normalMaterial.baseMaterial);
+
+        render.SetMaterials(materiallist);
+    }
+
+    [Serializable]
+    public struct materials
+    {
+        public Material baseMaterial;
+        public Material skinMaterial;
+    }
+}
