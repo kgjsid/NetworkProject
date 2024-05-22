81efe3a86dfbeb6fdb4930334b76094ceca76575
kgjsid <kgjsid@gmail.com>
2024-05-22 15:00:40 +0900

    fix : 아이템 사용방식 수정
    -> 네트워크에서 상대방과 나의 효과를 다르게 설정

--- Assets/_Seo/Scripts/ItemScripts/ItemObject.cs
+++ Assets/_Seo/Scripts/ItemScripts/ItemObject.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ItemObject : MonoBehaviour, IUseable
 {
+    /*
     // 실제 플레이어의 아이템 curItemType에 들고 있는 아이템을 사용할 수 있도록 설정
     // ItemType.None -> 아이템을 들고 있지 않다
     [SerializeField] ItemType curItemType;
@@ -57,7 +58,7 @@ public class ItemObject : MonoBehaviour, IUseable
 
         switch(curItemType)
         {
-            case ItemType.Trop:
+            case ItemType.Trap:
                 CaseTrop(user);
                 break;
             case ItemType.Shield:
@@ -86,15 +87,7 @@ public class ItemObject : MonoBehaviour, IUseable
     private void CaseTransparent(PlayerItemController user)
     {
         Debug.Log($"{user.Controller.name}이 투명화 아이템을 사용");
+        transparency.Use();
     }
-
-}
-
-public enum ItemType
-{   // 아이템을 표현할 열거형
-    None,
-    Trop,
-    Shield,
-    Transparent,
-    Size
+    */
 }
--- Assets/_Seo/Scripts/ItemScripts/Item_Transparency.cs
+++ Assets/_Seo/Scripts/ItemScripts/Item_Transparency.cs
@@ -4,14 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item_Transparency : MonoBehaviour
+public class Item_Transparency : MonoBehaviourPun
 {
     // 투명화 아이템
     [SerializeField] materials normalMaterial;
     [SerializeField] materials transparentMaterial;  // 완전 투명
     [SerializeField] materials translucentMaterial;  // 반투명
 
-    [SerializeField] PlayerController user;
+    [SerializeField] PlayerItemController user;
     [SerializeField] SkinnedMeshRenderer render;
 
     [SerializeField] float setTime;
@@ -21,29 +21,29 @@ public class Item_Transparency : MonoBehaviour
     [SerializeField] LayerMask translucentLayer;
     [SerializeField] LayerMask normalLayer;
 
-    public void SetUser(PlayerController user)
+    public void SetUser(PlayerItemController user)
     {
         this.user = user;
         render = user.gameObject.GetComponent<SkinnedMeshRenderer>();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SetTransparent();
-        }
-        else if(Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SetTranslucent();
-        }
-    }
-
     public void Use()
     {
-        
+        user.photonView.RPC("SetTransparent", RpcTarget.Others);
+        SetTranslucent();
     }
 
+    [PunRPC]
+    private void SetTransparent()
+    {   // 투명 설정
+        materiallist.Clear();
+        materiallist.Add(transparentMaterial.skinMaterial); materiallist.Add(transparentMaterial.baseMaterial);
+        //user.gameObject.layer = 30;
+
+        render.SetMaterials(materiallist);
+        StartCoroutine(SettingTime());
+    }
+    /*
     private void SetTransparent()
     {   // 불투명 설정
         materiallist.Clear();
@@ -52,7 +52,7 @@ public class Item_Transparency : MonoBehaviour
 
         render.SetMaterials(materiallist);
         StartCoroutine(SettingTime());
-    }
+    }*/
 
     private void SetTranslucent()
     {   // 반투명 설정
@@ -80,4 +80,5 @@ public class Item_Transparency : MonoBehaviour
         public Material baseMaterial;
         public Material skinMaterial;
     }
+
 }
